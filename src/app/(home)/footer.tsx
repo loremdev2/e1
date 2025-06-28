001030240e430080224af5aa809a93684fd6541d
loremdev2 <singanamalaaman@gmail.com>
2025-06-28 18:25:12 +0530

    Fix

--- src/app/(home)/footer.tsx
+++ src/app/(home)/footer.tsx
@@ -1,9 +1,11 @@
-export const Footer = () =>{
-    return (
-        <footer className="flex border-t justify-between font-medium p-6">
-            <div className="flex items-center gap-2">
-                <p>Lorem Ipsum</p>
-            </div>
-        </footer>
-    )
+export const Footer = () => {
+  return (
+    <footer className="flex flex-col sm:flex-row border-t justify-between items-center font-medium p-6 gap-4">      
+      <div className="flex items-center gap-2">
+        <p>© 2024 Lorem Ipsum</p>
+      </div>
+      <div className="flex items-center gap-4">
+      </div>
+    </footer>
+  )
 }
